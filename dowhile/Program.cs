db57a1feee372215f5c932c6df4e4183051795b9
Alina Meyn <akonova44@hotmail.com>
2018-09-30 23:43:11 +0200

    Add project files.

---
+++ dowhile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dowhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
